0c7d3cad9bca8216b9bdc05618cdbfb2d8f93525
Osamu Noguchi <noguchi@hws000617pc.local>
2019-03-08 10:21:53 +0900

    init

---
+++ nmlib.py
@@ -0,0 +1,167 @@
+import os, datetime, time, warnings
+
+from abc import ABCMeta, abstractmethod
+from pathlib import Path
+from contextlib import contextmanager
+import multiprocessing
+from multiprocessing import Pool
+
+import numpy as np
+import pandas as pd
+from pandas.api.types import CategoricalDtype
+from pandas.core.common import SettingWithCopyWarning
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from scipy.sparse import vstack, csr_matrix, save_npz, load_npz, hstack
+from gensim import corpora, models, similarities
+
+warnings.simplefilter(action='ignore', category=SettingWithCopyWarning)
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+pd.set_option("display.width", 1000)
+
+@contextmanager
+def timer(title):
+    t0 = time.time()
+    print(f'[{title}] start')
+    yield
+    print("{} - done in {:.0f}s".format(title, time.time() - t0))
+
+class FeatureEngineering(metaclass=ABCMeta):
+    BASE_DIR = "."
+    
+    def __init__(self):
+        self.name = self.__class__.__name__
+        self.train = pd.DataFrame()
+        self.test = pd.DataFrame()
+        self.train_file_path = f"{Path(self.BASE_DIR)}/{self.name.lower()}_train"
+        self.test_file_path = f"{Path(self.BASE_DIR)}/{self.name.lower()}_test"
+    
+    @abstractmethod
+    def load_data(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def create_features(self):
+        raise NotImplementedError
+        
+    def run(self, use_columns=[], skip_columns=[]):
+        with timer(self.name):
+            self.load_data()
+            self.replace_na(use_columns=use_columns, skip_columns=skip_columns)
+            self.onehot_encode(use_columns=use_columns, skip_columns=skip_columns)
+            self.create_features()
+        
+        return self
+    
+    def onehot_encode(self, use_columns=[], skip_columns=[], sparse=False):
+        use_columns = use_columns if use_columns else [c for c in self.train.columns if c not in skip_columns]
+        self.label_encode(use_columns, skip_columns)
+        if sparse:
+            encoder = OneHotEncoder(categories='auto', sparse=sparse, dtype='uint8').fit(pd.concat([self.train.loc[:, use_columns], self.test.loc[:, use_columns]]))
+            m = 100000
+            train = vstack([encoder.transform(self.train[i*m:(i+1)*m].loc[:, use_columns]) for i in range(self.train.shape[0] // m + 1)])
+            test  = vstack([encoder.transform(self.test[i*m:(i+1)*m].loc[:, use_columns])  for i in range(self.test.shape[0] // m +  1)])
+            save_npz(f"{self.train_file_path}.npz", train, compressed=True)
+            save_npz(f"{self.test_file_path}.npz",  test,  compressed=True)
+        else:
+            for col in use_columns:
+                self.train = self.train.join(pd.get_dummies(self.train[col], prefix=col))
+                self.test = self.test.join(pd.get_dummies(self.test[col], prefix=col))
+    
+    def label_encode(self, use_columns=[], skip_columns=[]):
+        use_columns = use_columns if use_columns else [c for c in self.train.columns if c not in skip_columns]
+        for col in use_columns:
+            self.train[col] = self.train[col].astype('str')
+            self.test[col] = self.test[col].astype('str')
+            
+            le = LabelEncoder().fit(np.unique(self.train[col].unique().tolist()+self.test[col].unique().tolist()))
+            self.train[col] = le.transform(self.train[col])+1
+            self.test[col]  = le.transform(self.test[col])+1
+    
+        return self
+    
+    def agg_transform(self, group, agg, prefix=""):
+        prefix = f"{prefix}_" if prefix else ""
+        for k, v in agg.items():
+            if type(v) is str:
+                self.train[f"{prefix}{k}_{v}"] = self.train.groupby(group)[k].transform(v)
+                self.test[f"{prefix}{k}_{v}"] = self.test.groupby(group)[k].transform(v)
+            else:
+                for vv in v:
+                    self.train[f"{prefix}{k}_{vv}"] = self.train.groupby(group)[k].transform(vv)
+                    self.test[f"{prefix}{k}_{vv}"] = self.test.groupby(group)[k].transform(vv)
+        
+        return self
+    
+    def replace_na(self, use_columns=[], skip_columns=[], fill_value=-1):
+        use_columns = use_columns if use_columns else [c for c in self.train.columns if c not in skip_columns]
+        for col in use_columns:
+            if isinstance(self.train[col].dtype, CategoricalDtype):
+                self.train[col] = self.train[col].cat.add_categories(str(fill_value)).replace(np.inf, np.nan).replace(-np.inf, np.nan).fillna(str(fill_value))
+                self.test[col] = self.test[col].cat.add_categories(str(fill_value)).replace(np.inf, np.nan).replace(-np.inf, np.nan).fillna(str(fill_value))
+            else:
+                self.train[col] = self.train[col].replace(np.inf, np.nan).replace(-np.inf, np.nan).fillna(fill_value)
+                self.test[col] = self.test[col].replace(np.inf, np.nan).replace(-np.inf, np.nan).fillna(fill_value)
+
+        return self
+    
+    def calc_topic_score(self, topic_text_columns, num_topics=5):
+        df = pd.concat([self.train.loc[:, topic_text_columns], self.test.loc[:, topic_text_columns]])
+        
+        for col in topic_text_columns:
+            texts = [[word for word in document.lower().split()] for document in df[col].values]
+            dictionary = corpora.Dictionary(texts)
+            bow_corpus = [dictionary.doc2bow(t) for t in texts]
+            lda = models.LdaModel(bow_corpus, id2word=dictionary, num_topics=num_topics)
+                        
+            size = df.shape[0]
+            topics = {i:[-1]*size for i in range(num_topics)}
+            for i, row in enumerate(lda[bow_corpus]):
+                for j, (topic_num, prop_topic) in enumerate(row):
+                    topics[topic_num][i] = prop_topic
+            
+            for i in range(num_topics):
+                self.train[f"{col}_topic_{i}"] = topics[i][:self.train.shape[0]]
+                self.test[f"{col}_topic_{i}"] = topics[i][self.train.shape[0]:]
+
+        return self
+    
+    def columns_1d(self):
+        self.train.columns = pd.Index([(e[0] + "_" + e[1].lower()) if (len(e[1]) > 0) else e[0] for e in self.train.columns.tolist()])
+        self.test.columns = pd.Index([(e[0] + "_" + e[1].lower()) if (len(e[1]) > 0) else e[0] for e in self.test.columns.tolist()])
+
+        return self
+    
+    def head(self, title="", columns=[], limit=5):
+        train_cols, test_cols = (columns, columns) if columns else (self.train.columns, self.test.columns)
+        
+        print(f"train head: {title}")
+        print(self.train.loc[:, train_cols].head(limit))
+        print("----------------------------")
+        print(f"test head: {title}")
+        print(self.test.loc[:, test_cols].head(limit))
+        print("----------------------------")
+        
+        return self
+    
+    def tail(self, title="", columns=[], limit=5):
+        train_cols, test_cols = (columns, columns) if columns else (self.train.columns, self.test.columns)
+        
+        print(f"train tail: {title}")
+        print(self.train.loc[:, train_cols].tail(limit))
+        print("----------------------------")
+        print(f"test tail: {title}")
+        print(self.test.loc[:, test_cols].tail(limit))
+        print("----------------------------")
+        
+        return self
+    
+    def save(self, format="feather", index=False):
+        if format == "feather":
+            self.train.to_feather(f"{self.train_file_path}.ftr")
+            self.test.to_feather(f"{self.test_file_path}.ftr")
+        elif format == "csv":
+            self.train.to_csv(f"{self.train_file_path}.csv", index=index)
+            self.test.to_csv(f"{self.test_file_path}.csv", index=index)
+        
+        return self
